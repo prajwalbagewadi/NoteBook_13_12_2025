127e0712a199d563185caffafb2ea3594727e99c
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2025-12-26 13:49:09 +0530

    update 26thDec2025

--- ChatApp_Idea/backend/Service/SessionService.java
+++ ChatApp_Idea/backend/Service/SessionService.java
@@ -1,5 +1,8 @@
 package com.example.api.Service;
 
+import com.example.api.Model.Person;
+import com.example.api.Model.WaitQueue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.UUID;
 /*
@@ -10,10 +13,24 @@ like sessions, users, requests, tokens, etc.
 
 @Service
 public class SessionService {
+
+    @Autowired
+    WaitQueue wq;
+
     private String sessionId;
     public String generateSessionId(){
         sessionId = UUID.randomUUID().toString();
         System.out.println("UUID:"+sessionId);
         return sessionId;
+    }
+
+    public String addToWaitQueue(Person person){
+        if(!wq.verifyUniqueId(person)){
+            return "response: Person already WaitQueue.";
+        }
+        wq.insert(person);
+        wq.print();
+        wq.count();
+        return "response: Person added to WaitQueue.";
     }
 }

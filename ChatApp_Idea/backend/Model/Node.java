f2b5961c632ec7269c11fdd5c7abb7b21affb421
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2025-12-26 13:50:38 +0530

    Add Node class for linked list implementation update 26thDec2025

---
+++ ChatApp_Idea/backend/Model/Node.java
@@ -0,0 +1,12 @@
+package com.example.api.Model;
+
+public class Node {
+    Person person; //data stored in node.
+    Node next; //reference to next node.
+
+    //Constructor to create a node with data
+    public Node(Person person){
+        this.person=person;
+        this.next=null; //Next is null by default for a new last node
+    }
+}

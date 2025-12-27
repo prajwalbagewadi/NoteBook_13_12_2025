355cce22771764f088c4d95f337525e0f159c2a7
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2025-12-27 14:04:38 +0530

    Add person matching and update wait queue logic and matchperson logic update 27dec2025

--- ChatApp_Idea/backend/Service/SessionService.java
+++ ChatApp_Idea/backend/Service/SessionService.java
@@ -1,9 +1,12 @@
 package com.example.api.Service;
 
+import com.example.api.Model.Node;
 import com.example.api.Model.Person;
 import com.example.api.Model.WaitQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 import java.util.UUID;
 /*
 UUID (Universally Unique Identifier),
@@ -24,13 +27,46 @@ public class SessionService {
         return sessionId;
     }
 
-    public String addToWaitQueue(Person person){
-        if(!wq.verifyUniqueId(person)){
-            return "response: Person already WaitQueue.";
+    public String matchPerson(Person person1,Person person2){
+        if(person1.getSessionId()==null || person2.getSessionId()==null){
+            System.out.println("errorLog:no SessionId present");
         }
-        wq.insert(person);
-        wq.print();
-        wq.count();
-        return "response: Person added to WaitQueue.";
+        person1.setPartnerId(person2.getSessionId());
+        person2.setPartnerId(person1.getSessionId());
+        return person1.getPartnerId()+"-matchedWith-"+person2.getPartnerId();
+    }
+
+    public String addToWaitQueue(Person inPerson){
+        String res="";
+        System.out.println("log:"+inPerson.getSessionId());
+        if(wq.isEmpty()){
+            wq.insert(inPerson);
+            wq.print();
+            wq.count();
+            res="You are waiting in Queue";
+        } else {
+            int count=0;
+            for(Node n = wq.ilterator();n!=null;n=n.getNext()) {
+                if (inPerson.getTopic().equals(n.getPerson().getTopic())) {
+                    //match first.
+                    res = matchPerson(inPerson, n.getPerson());
+                    System.out.println("count:" + count);
+                    //delete
+                    Node nq = wq.delete(count);
+                    //res="matched with"+nq.getPerson().getSessionId();
+                    //break loop
+                    break;
+                }
+                count++;
+            }
+        }
+//        if(!wq.verifyUniqueId(person)){
+//            return "response: Person already WaitQueue.";
+//        }
+//        wq.insert(person);
+//        wq.print();
+//        wq.count();
+//        return "response: Person added to WaitQueue.";
+        return res;
     }
 }

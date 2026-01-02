c51b6a96078edc560816320c1ed221cc6a26af80
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2026-01-02 15:22:58 +0530

    chat screen with send() and recieve() msg 02jan2026

--- ChatApp_Idea/frontend/TempApp.js
+++ ChatApp_Idea/frontend/TempApp.js
@@ -7,11 +7,87 @@ import {
   Image,
 } from "react-native";
 import { useFonts } from "expo-font";
-
+import { useState, useEffect } from "react";
 export default function TempApp() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]); //list of messages
+  //  const [rmessages, rSetMessages] = useState([]); //list of messages
   const [fontLoaded] = useFonts({
     Ndot: require("./assets/fonts/ndot-47-inspired-by-nothing.otf"),
   });
+  //receiveMessage API function
+  const recieveMessage = async function (params) {
+    console.log("recieve msg().");
+    try {
+      const payload = {
+        sessionId: "123",
+      };
+      const response = await fetch(
+        "http://192.168.30.128:8080/receiveMessage",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload),
+        }
+      );
+      const data = await response.json();
+      console.log("response from api:", data);
+      if (data && data.message) {
+        setMessages((prev) => [
+          ...prev,
+          {
+            id: Date.now().toString(),
+            senderId: data.senderId,
+            message: data.message,
+            timestamp: data.timeStamp,
+          },
+        ]);
+      }
+    } catch (err) {
+      console.log("recieve msg api:", err);
+    }
+  };
+  //Call receiveMessage using useEffect (Polling)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      recieveMessage();
+    }, 2000); //poll every 2secs
+    return () => clearInterval(interval);
+  }, []);
+  const sendMessage = async function () {
+    console.log("send msg pressed.");
+    try {
+      const payload = {
+        senderId: "123",
+        receiverId: "456",
+        message: message,
+      };
+      const response = await fetch("http://192.168.30.128:8080/sendMessage", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+      // Add the new message to the existing messages list using the previous state
+      setMessages((prev) => [
+        ...prev,
+        {
+          id: Date.now().toString(), //unique id for list
+          senderId: "123",
+          message: message,
+          timestamp: new Date().toISOString(), // ⏰ store time
+        },
+      ]);
+      setMessage("");
+      const data = await response.json();
+      console.log("response from api:", data);
+    } catch (err) {
+      console.log("api error", err);
+    }
+  };
+  const backButton = function () {
+    console.log("Back pressed.");
+    //navigation.navigate("Screen1");
+  };
   if (!fontLoaded) {
     return (
       <View style={{ fontFamily: "Ndot", fontSize: 24, color: "#0f0f0f" }}>
@@ -39,10 +115,101 @@ export default function TempApp() {
         Chat Room.
       </Text>
       <FlatList
+        data={messages}
+        keyExtractor={(item) => item.id} //keyExtractor gives FlatList a unique key for each item to render and update efficiently //item = one message/element //item.id = unique key
+        // renderItem={({ item }) => {
+        //   return (
+        //     <View
+        //       style={{
+        //         alignItems: "flex-end",
+        //         marginVertical: 6,
+        //       }}
+        //     >
+        //       <View
+        //         style={{
+        //           backgroundColor: "#860808",
+        //           paddingVertical: 8,
+        //           paddingHorizontal: 14,
+        //           borderRadius: 18,
+        //           maxWidth: "75%",
+        //         }}
+        //       ></View>
+        //       <Text
+        //         style={{ color: "#F7F7F7", fontSize: 16, fontFamily: "Ndot" }}
+        //       >
+        //         {item.message}
+        //       </Text>
+        //       <Text
+        //         style={{
+        //           fontSize: 10,
+        //           fontFamily: "Ndot",
+        //           color: "#eaeaea",
+        //           alignSelf: "flex-end",
+        //           marginTop: 4,
+        //         }}
+        //       >
+        //         {item.timestamp}
+        //       </Text>
+        //     </View>
+        //   );
+        // }}
+        renderItem={({ item }) => {
+          const isMe = item.senderId === "123";
+          return (
+            <View
+              style={{
+                alignItems: isMe ? "flex-end" : "flex-start",
+                marginVertical: 6,
+                paddingHorizontal: 10,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: isMe ? "#1a1a1a" : "#B60808",
+                  paddingVertical: 10,
+                  paddingHorizontal: 20,
+                  borderRadius: 18,
+                  maxWidth: "75%",
+                }}
+              >
+                <Text
+                  style={{
+                    color: "#F7F7F7",
+                    fontSize: 16,
+                    fontFamily: "Ndot",
+                  }}
+                >
+                  {item.senderId}
+                </Text>
+                <Text
+                  style={{
+                    color: "#F7F7F7",
+                    fontSize: 16,
+                    fontFamily: "Ndot",
+                  }}
+                >
+                  {item.message}
+                </Text>
+
+                <Text
+                  style={{
+                    fontSize: 10,
+                    fontFamily: "Ndot",
+                    color: "#f7f7f7",
+                    alignSelf: "flex-end",
+                    marginTop: 4,
+                  }}
+                >
+                  {new Date(item.timestamp).toLocaleTimeString()}
+                </Text>
+              </View>
+            </View>
+          );
+        }}
         style={{
           marginTop: 10,
           borderRadius: 70,
-          backgroundColor: "#f0f0f0",
+          backgroundColor: "#1a1a1a",
           margin: 5,
           width: 400,
           height: 500,
@@ -57,6 +224,8 @@ export default function TempApp() {
       >
         {/* Text Input */}
         <TextInput
+          value={message}
+          onChangeText={setMessage}
           multiline={true}
           scrollEnabled={true}
           style={{
@@ -77,7 +246,7 @@ export default function TempApp() {
 
         {/* Send Button */}
         <TouchableOpacity
-          onPress={() => console.log("Send pressed")}
+          onPress={sendMessage}
           style={{
             marginLeft: 10,
             padding: 6,
